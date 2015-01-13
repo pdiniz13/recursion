5f2ed2af231838e4a348a4305968d8ed9db13de9
Paulo Diniz <pdiniz13@gmail.com>
2015-01-13 11:20:48 -0800

    Modified getElementsByClassName to meet project specs. Function is now using className, document.body, and childNodes

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -5,16 +5,12 @@ describe('stringifyJSON', function(){
     stringifiableObjects.forEach(function(test){
       var expected = JSON.stringify(test);
       var result = stringifyJSON(test);
-      console.log(expected);
-      console.log(result);
       expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj){
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-      console.log(expected);
-      console.log(result);
       expect(result).to.equal(expected);
     });
 
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,21 +4,45 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var x = 0;
-var HTMLCollection = [];
-var getElementsByClassName = function(className){
-    var newClassName = "."+className;
-    var returnValue = $(document).find(newClassName);
-        if (returnValue.length > x) {
-            HTMLCollection[x]=returnValue[x];
-            x++;
-            getElementsByClassName(className);
+var getElementsByClassName = function(className2, newArray, returnValue){
+    if (newArray === undefined){
+        newArray = [];
+        newArray.push(document.body);
+    }
+    if (returnValue === undefined){
+        returnValue = [].slice.apply(document.body.childNodes);
+    }
+    if (returnValue.length > 0) {
+        var newValue = returnValue.shift();
+        if (newValue !== undefined && newValue.className !== undefined && newValue.className.indexOf(className2) > -1){
+            newArray.push(newValue);
         }
-        else {
-            x=0;
-            if(returnValue.length < HTMLCollection.length){
-                HTMLCollection = HTMLCollection.slice(0,returnValue.length);
-            }
+        if (newValue.hasChildNodes()){
+            var newValue2 = [].slice.apply(newValue.childNodes);
+            getElementsByClassName(className2, newArray, newValue2);
         }
-    return HTMLCollection;
-    };
+        getElementsByClassName(className2, newArray, returnValue);
+    }
+    return newArray;
+};
+
+
+
+//var x = 0;
+//var HTMLCollection = [];
+//var getElementsByClassName = function(className){
+//    var newClassName = "."+className;
+//    var returnValue = $(document).find(newClassName);
+//        if (returnValue.length > x) {
+//            HTMLCollection[x]=returnValue[x];
+//            x++;
+//            getElementsByClassName(className);
+//        }
+//        else {
+//            x=0;
+//            if(returnValue.length < HTMLCollection.length){
+//                HTMLCollection = HTMLCollection.slice(0,returnValue.length);
+//            }
+//        }
+//    return HTMLCollection;
+//    };
